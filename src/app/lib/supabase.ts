d2f53752570fe6d97e41103ad6bb9d376dc821b1
pawel.zaton17 <pawzatkontakt@gmail.com>
2025-06-15 19:18:12 +0200

    removed logs

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -3,9 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-console.log("→ SUPABASE_URL:", supabaseUrl);
-console.log("→ SUPABASE_ANON_KEY:", supabaseAnonKey?.slice(0, 10) + "...");
-
 /**
  * Supabase client instance for interacting with the database.
  */
