bdf0f47368837ec44561204b2b3d735109656d1c
Sebastian Ślęczka <me@sevastijan.com>
2025-11-09 16:37:52 +0100

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
-console.log(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
 export const authOptions: NextAuthOptions = {
      providers: [
           GoogleProvider({
