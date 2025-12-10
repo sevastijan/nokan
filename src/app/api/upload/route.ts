91774cb59ade36c223bf0adf36c3085e16aa21d4
pawel.zaton17 <pawzatkontakt@gmail.com>
2025-12-10 01:29:28 +0100

    Test

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -2,6 +2,9 @@ import { createClient } from '@supabase/supabase-js';
 import { getToken } from 'next-auth/jwt';
 import { NextRequest } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+export const runtime = 'nodejs';
+
 const supabaseAdmin = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SERVICE_ROLE_KEY!, {
      auth: {
           autoRefreshToken: false,
